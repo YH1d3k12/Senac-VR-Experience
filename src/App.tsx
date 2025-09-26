47eb5a1bdb200d2c1a5c6a214a3ecd0da8fc70b4
Juma <djalmahy@hotmail.com>
2025-09-26 20:57:55 -0300

    Deployed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './styles/App.css';
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename='/Senac-VR-Experience/'>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
